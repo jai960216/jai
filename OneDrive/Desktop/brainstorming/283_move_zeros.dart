2f5b5a9b04a410e5064b883918fbe76832ff2b1f
jai960216 <jai96021@gmail.com>
2025-03-14 10:33:30 +0900

    Add files via upload

---
+++ OneDrive/Desktop/brainstorming/283_move_zeros.dart
@@ -0,0 +1,13 @@
+class Solution {
+  void moveZeroes(List<int> nums) {
+    int left = 0;
+    for(int right = 0; right < nums.length; right++){
+        if (nums[right] != 0){
+            int tmp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = tmp;
+            left++;
+        }
+    }
+  }
+}
